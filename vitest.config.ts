d49d18080fc3313b67f927269f521829bf697b0e
Oscar Alan Pierce <oscar.alan.pierce@gmail.com>
2026-07-25 12:29:56 +1000

    Configure test coverage reporting to run in CI

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,20 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-  }
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'json-summary', 'html'],
+      include: ['src/**'],
+      exclude: [
+        '**/types/**',
+        'src/main.tsx',
+      ],
+      thresholds: {
+        lines: 95,
+        functions: 95,
+        branches: 95,
+        statements: 95,
+      },
+    },
+  },
 })
