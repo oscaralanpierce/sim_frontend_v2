7606efb3a210d2492d0d8ecf048f86b0a8a3ba94
Oscar Alan Pierce <oscar.alan.pierce@gmail.com>
2026-07-25 10:58:40 +1000

    Create new TypeScript 7/Vite 8/React 19 app

---
+++ src/App.test.tsx
@@ -0,0 +1,12 @@
+// TODO: Add Vitest
+
+// import { describe, test, expect } from 'vitest'
+// import { render } from '@testing-library/react'
+// import App from './App'
+
+// describe('<App />', () => {
+//   test('App mounts properly', () => {
+//     const wrapper = render(<App />)
+//     expect(wrapper).toBeTruthy()
+//   })
+// })
