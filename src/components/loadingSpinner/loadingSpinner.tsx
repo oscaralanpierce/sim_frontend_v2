56cd2969c585359eea6ef050de77e432a31a2973
Oscar Alan Pierce <oscar.alan.pierce@gmail.com>
2026-08-06 08:06:50 +1000

    Use semantic HTML for loading dialog and add tests for color context (not sure why they were failing CI now and not in the PR that added that context but oh well)

--- src/components/loadingSpinner/loadingSpinner.tsx
+++ src/components/loadingSpinner/loadingSpinner.tsx
@@ -16,7 +16,7 @@ const LoadingSpinner = () => {
 
   return (
     <div className={styles.root} style={styleVars}>
-      <p className={styles.text}>Loading...</p>
+      <dialog className={styles.text}>Loading...</dialog>
       <div className={styles.spinner} role="progressbar" aria-label="loading">
         <span className={styles.spinnerComponent} key="first"></span>
         <span className={styles.spinnerComponent} key="second"></span>
